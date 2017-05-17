2193f502c1417420ff0a380d5e461121ae75697e
lcojunk <lcojunk@gmail.com>
2017-05-17 13:17:41 +0200

    formated hide and show examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { InfoBoxComponent } from './components/info-box/info-box.component';
-import { MouseCursorComponent } from './components/mouse-cursor/mouse-cursor.component';
-import { TitleBoxComponent } from './components/title-box/title-box.component';
-
-
-import { routing } from "./app.routing";
-import { TooltipDirective } from './shared/tooltip.directive';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { routing } from "./app.routing";
 
 //import { BookModule } from "app/book/book.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    InfoBoxComponent,
-    MouseCursorComponent,
-    TitleBoxComponent,
-    TooltipDirective,
     WelcomePageComponent
   ],
   imports: [
