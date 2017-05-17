7b43c287fc56c11527a08bd2cb7c602e4a992245
lcojunk <lcojunk@gmail.com>
2017-05-17 09:37:15 +0200

    added hide and show module

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,8 @@ import { NgModule }              from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: '/books'},
-    {path: 'books', loadChildren: './book/book.module#BookModule'}
+//    { path: '', pathMatch: 'full', redirectTo: '/books' },
+    { path: 'books', loadChildren: './book/book.module#BookModule' },
+    { path: 'hide-n-show', loadChildren: './hide-n-show/hide-n-show.module#HideNShowModule' }
   ];
 export const routing = RouterModule.forRoot(routes);
